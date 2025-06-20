939ec15c274b88eb2d21965f06bf2a76372b757c
Saidosik <Sadiliofimperia@gmail.com>
2025-06-20 14:00:14 +0300

    SAID GO

--- scripts/hero-slider.js
+++ scripts/hero-slider.js
@@ -1,20 +1,45 @@
 const images = [
-    'images/hero/slide1.png',
-    'images/hero/slide2.png',
-    'images/hero/slide3.png',
-    'images/hero/slide4.png',
-    'images/hero/slide5.png',
-    'images/hero/slide6.png',
-    'images/hero/slide7.png'
-  ];
-  
-  let index = 0;
-  const slider = document.querySelector('.hero-slider');
-  
-  function changeBackground() {
-    slider.style.backgroundImage = `url('${images[index]}')`;
-    index = (index + 1) % images.length;
-  }
-  
-  changeBackground(); // Установка первого изображения
-  setInterval(changeBackground, 1000); // Меняем каждые 5 секунд
+  'images/hero/slide1.png',
+  'images/hero/slide2.png',
+  'images/hero/slide3.png',
+  'images/hero/slide4.png',
+  'images/hero/slide5.png',
+  'images/hero/slide6.png'
+];
+
+let index = 0;
+const slider = document.querySelector('.hero-slider');
+
+function updateSlider() {
+  slider.style.backgroundImage = `url('${images[index]}')`;
+}
+
+function nextSlide() {
+  index = (index + 1) % images.length;
+  updateSlider();
+  resetInterval();
+}
+
+function prevSlide() {
+  index = (index - 1 + images.length) % images.length;
+  updateSlider();
+  resetInterval();
+}
+
+// Таймер автоматического переключения
+let intervalId = setInterval(nextSlide, 10000);
+
+function resetInterval() {
+  clearInterval(intervalId);
+  intervalId = setInterval(nextSlide, 10000);
+}
+
+// Кнопки
+let heroContinue = document.getElementById('hero-continue');
+let heroPrevous = document.getElementById('hero-prevous');
+
+heroContinue.addEventListener('click', nextSlide);
+heroPrevous.addEventListener('click', prevSlide);
+
+// Установка первого изображения
+updateSlider();
