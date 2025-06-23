b30ed26b7f535b24ffa4032ab4080db3106b20ea
Saidosik <Sadiliofimperia@gmail.com>
2025-06-23 13:16:34 +0300

    Update validation.js

--- scripts/validation.js
+++ scripts/validation.js
@@ -29,7 +29,7 @@ document.querySelectorAll('.contact-form').addEventListener('submit', function(e
   });
 
 let nameField = document.querySelector("input[type=text]")
-let tel = document.querySelector("input[type=tel]")Add commentMore actions
+let tel = document.querySelector("input[type=tel]")
 let sentBtn = document.querySelector("#sent")
 let telMask = new Inputmask("+7(999) 999-99-99")
 telMask.mask(tel)
