b50c91556857d53ad43028f4c35c61debdf85664
kyusak0 <chirkinvova4@gmail.com>
2025-06-23 09:22:17 +0300

    тест коммит

--- scripts/database.js
+++ scripts/database.js
@@ -2,51 +2,51 @@ const products = [
     {
         title: "Relle vinyl tiles",
         type: "Виниловые покрытия",
-        image: "../images/catalog/vinyl.png",
+        image: "images/catalog/vinyl.png",
     }, {
         title: "Yibel Wooden",
         type: "Гетерагенные напольные покрытия",
-        image: "../images/catalog/heterogeneous.png",
+        image: "images/catalog/heterogeneous.png",
     }, {
         title: "Canna",
         type: "Гомогенные напольные покрытия",
-        image: "../images/catalog/Canna.png",
+        image: "images/catalog/Canna.png",
     }, {
         title: "Jasmine",
         type: "Гомогенные напольные покрытия",
-        image: "../images/catalog/jasmine.png",
+        image: "images/catalog/jasmine.png",
     }, {
         title: "Canna",
         type: "Гомогенные напольные покрытия",
-        image: "../images/catalog/Canna.png",
+        image: "images/catalog/Canna.png",
     }, {
         title: "Relle vinyl tiles",
         type: "Виниловые покрытия",
-        image: "../images/catalog/vinyl.png",
+        image: "images/catalog/vinyl.png",
     }, {
         title: "Jasmine",
         type: "Гомогенные напольные покрытия",
-        image: "../images/catalog/jasmine.png",
+        image: "images/catalog/jasmine.png",
     }, {
         title: "Yibel Wooden",
         type: "Гетерагенные напольные покрытия",
-        image: "../images/catalog/heterogeneous.png",
+        image: "images/catalog/heterogeneous.png",
     },
 ];
 
 const certificates = [
     {
         title: "Сертификат SGS - Однородные полы из ПВХ (RoHS)",
-        image: "../images/certificates/certif1.png",
+        image: "images/certificates/certif1.png",
     }, {
         title: "Сертификат SGS-ПВХ однородный пол (антистатический)",
-        image: "../images/certificates/certif2.png",
+        image: "images/certificates/certif2.png",
     }, {
         title: "Сертификат Intertek-ПВХ однородный пол (антибактериальный)",
-        image: "../images/certificates/certif3.png",
+        image: "images/certificates/certif3.png",
     }, {
         title: "Сертификат CE - Гетерогенные полы из ПВХ",
-        image: "../images/certificates/certif4.png",
+        image: "images/certificates/certif4.png",
     },
 ];
 
@@ -54,19 +54,19 @@ const portfolio = [
     {
         title: "",
         type: "Тут нужно написать краткую информацию о объекте, возможно указать адрес",
-        image: "../images/our-works/our works1.png",
+        image: "images/our-works/our works1.png",
     }, {
         title: "",
         type: "Тут нужно написать краткую информацию о объекте, возможно указать адрес",
-        image: "../images/our-works/our works2.png",
+        image: "images/our-works/our works2.png",
     }, {
         title: "",
         type: "Тут нужно написать краткую информацию о объекте, возможно указать адрес",
-        image: "../images/our-works/our works3.png",
+        image: "images/our-works/our works3.png",
     }, {
         title: "",
         type: "Тут нужно написать краткую информацию о объекте, возможно указать адрес",
-        image: "../images/our-works/our works4.png",
+        image: "images/our-works/our works4.png",
     },
 ];
 
@@ -74,23 +74,23 @@ const coverings = [
     {
         title: "Образовательные объекты",
         type: "Работаем по 10+ странам. Офисы в 15 горох России и за рубежом. Склады в 3х странах",
-        image: "../images/coverings/education covering.png",
+        image: "images/coverings/education covering.png",
     }, {
         title: "Склады и промышленные объекты",
         type: "Работаем по 10+ странам. Офисы в 15 горох России и за рубежом. Склады в 3х странах",
-        image: "../images/coverings/education covering.png",
+        image: "images/coverings/education covering.png",
     }, {
         title: "Медицинские объекты",
         type: "Работаем по 10+ странам. Офисы в 15 горох России и за рубежом. Склады в 3х странах",
-        image: "../images/coverings/medicine covering.png",
+        image: "images/coverings/medicine covering.png",
     }, {
         title: "Спортивные объекты",
         type: "Работаем по 10+ странам. Офисы в 15 горох России и за рубежом. Склады в 3х странах",
-        image: "../images/coverings/stadion covering.png",
+        image: "images/coverings/stadion covering.png",
     }, {
         title: "Ковровые покрытия",
         type: "Работаем по 10+ странам. Офисы в 15 горох России и за рубежом. Склады в 3х странах",
-        image: "../images/coverings/carpet covering.png",
+        image: "images/coverings/carpet covering.png",
     },
 ];
 
